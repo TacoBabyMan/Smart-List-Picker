2dd42efb67afcee2b9c1c94fdf13cd497a94e8a3
TacoBabyMan <tacobabyman@gmail.com>
2022-03-29 08:30:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const readline = require('readline');
+
+let 
+
+let dataSet = [];
+let randomNumber = (dataSet.length>=1)?Math.floor(Math.random()*dataSet.length):"Nothing found in list";
+
+console.log(randomNumber);
